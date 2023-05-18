3c27422b1f2ee7b16c9e2a2214078a113b16442a
Your Name <you@example.com>
2023-05-18 09:44:17 +0330

    fix build issues

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  output: {
-    exportTrailingSlash: true,
-  },
+  trailingSlash: true,
+  output: 'export',
 }
 
 module.exports = nextConfig
